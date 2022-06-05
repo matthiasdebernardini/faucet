9a868d214cdeef656c578c711bd30b9e4a31e8a5
Justin Moon <mail@justinmoon.com>
2022-06-04 20:46:00 -0500

    Form submission creates lightning invoice

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 import os
+import random
 
-from flask import Flask
+from flask_qrcode import QRcode
+from flask import Flask, render_template, request
 from lightning import LightningRpc
 
-rpc = LightningRpc(os.environ["RPC_SOCKET"])
+rpc = LightningRpc(os.environ['RPC_SOCKET'])
 app = Flask(__name__)
+QRcode(app)
 
-@app.route("/")
+@app.route('/getinfo')
 def getinfo():
     return str(rpc.getinfo())
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    invoice = None
+    pay_result = None
+
+    # set invoice if that was requested
+    if request.method == 'POST':
+        amount = request.form['amount']
+        print(request.form)
+        invoice = rpc.invoice(amount, str(random.random()), 'test')
+
+    return render_template('index.html', name='justin', invoice=invoice)
